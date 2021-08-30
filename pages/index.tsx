003837fe87c18d8970d9740089b8c292b40aa993
Wilson Cusack <cusackw@gmail.com>
2021-08-30 10:31:45 -0400

    comment out old code

--- pages/index.tsx
+++ pages/index.tsx
@@ -383,119 +383,119 @@ function MintingSection({info}){
   )
 }
 
-function MintController({mintFee, mintCallBack}) {
-  const [artTransferContractWeb3, setArtTransferContractWeb3] = React.useState(null)
-  const [providerAvailable, setProviderAvailable] = useState(null)
-  const [account, setAccount] = React.useState(null)
-  const [transactionHash, setTransactionHash] = useState("")
-  const [id, setId] = useState(null)
+// function MintController({mintFee, mintCallBack}) {
+//   const [artTransferContractWeb3, setArtTransferContractWeb3] = React.useState(null)
+//   const [providerAvailable, setProviderAvailable] = useState(null)
+//   const [account, setAccount] = React.useState(null)
+//   const [transactionHash, setTransactionHash] = useState("")
+//   const [id, setId] = useState(null)
 
-  const setup = async () => {
-    if(window.ethereum == null){
-      setProviderAvailable(false)
-      return
-    }
-    setProviderAvailable(true)
+//   const setup = async () => {
+//     if(window.ethereum == null){
+//       setProviderAvailable(false)
+//       return
+//     }
+//     setProviderAvailable(true)
 
-    const provider = new ethers.providers.Web3Provider(window.ethereum)
-    setArtTransferContractWeb3(createWeb3ArtTransfers(provider))
-  }
+//     const provider = new ethers.providers.Web3Provider(window.ethereum)
+//     setArtTransferContractWeb3(createWeb3ArtTransfers(provider))
+//   }
 
-  const createWeb3ArtTransfers = (provider) => {
-    return new ethers.Contract(
-     process.env.NEXT_PUBLIC_CONTRACT,
-     TransferArtArtifact.abi,
-     provider.getSigner(0)
-   );
-  }
+//   const createWeb3ArtTransfers = (provider) => {
+//     return new ethers.Contract(
+//      process.env.NEXT_PUBLIC_CONTRACT,
+//      TransferArtArtifact.abi,
+//      provider.getSigner(0)
+//    );
+//   }
 
-  const getAccount = async () => {
-    const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' });
-    const account = accounts[0]
-    setAccount(account)
-    window.ethereum.on('accountsChanged', function (accounts) {
-      console.log("accounts changed")
-      setAccount(accounts[0])
-    })
-  }
+//   const getAccount = async () => {
+//     const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' });
+//     const account = accounts[0]
+//     setAccount(account)
+//     window.ethereum.on('accountsChanged', function (accounts) {
+//       console.log("accounts changed")
+//       setAccount(accounts[0])
+//     })
+//   }
 
-  const mint = async () => {
-    setTransactionHash("")
-    setId(null)
-    const t = await artTransferContractWeb3.mint(account, {value: ethers.utils.parseUnits(mintFee, 18)})
-    setTransactionHash(t.hash)
-    t.wait().then((receipt) => {
-        waitForEvent()
-        mintCallBack()
-      })
-      .catch(err => {
-        console.log(err)
-      })
-  }
+//   const mint = async () => {
+//     setTransactionHash("")
+//     setId(null)
+//     const t = await artTransferContractWeb3.mint(account, {value: ethers.utils.parseUnits(mintFee, 18)})
+//     setTransactionHash(t.hash)
+//     t.wait().then((receipt) => {
+//         waitForEvent()
+//         mintCallBack()
+//       })
+//       .catch(err => {
+//         console.log(err)
+//       })
+//   }
 
-  const waitForEvent = async () => {
-    const filter = transferArtContract.filters.Transfer(null, account)
-    console.log(filter)
-    transferArtContract.once(filter, (from, to, id) => {
-      console.log("id!")
-      console.log(id.toString())
-      loadMedia(id)
-      setId(id.toString())
+//   const waitForEvent = async () => {
+//     const filter = transferArtContract.filters.Transfer(null, account)
+//     console.log(filter)
+//     transferArtContract.once(filter, (from, to, id) => {
+//       console.log("id!")
+//       console.log(id.toString())
+//       loadMedia(id)
+//       setId(id.toString())
       
-    }
-    )
-  }
+//     }
+//     )
+//   }
 
-  const loadMedia = async (id) => {
-    var x = await getNFTInfo({Contract: transferArtContract, tokenId: id})
-    console.log(x)
-  }
+//   const loadMedia = async (id) => {
+//     var x = await getNFTInfo({Contract: transferArtContract, tokenId: id})
+//     console.log(x)
+//   }
 
-  useEffect(() => {
-    setup()
-  }, [])
+//   useEffect(() => {
+//     setup()
+//   }, [])
 
-  return(
-    <div>
-      {
-      account != null ? 
-      <div className="btn" onClick={mint}> <img src="mint_button.svg" /> </div>:
-      <div>
-        {
-          providerAvailable == null ? 
-          ""
-          :
-          <div>
-          { providerAvailable ? 
-          <div className="btn" onClick={getAccount}> <img src="connect_wallet.svg" /> </div> : 
-          "In order to mint, please use Chrome + Metamask"
-        } 
-        </div>
-        }
-      </div>
-      }
-      {
-        transactionHash == "" ? "" :
-        <a target="_blank" href={process.env.NEXT_PUBLIC_ETHERSCAN_URL + "/tx/" +  transactionHash}> See transaction on Etherscan</a>
+//   return(
+//     <div>
+//       {
+//       account != null ? 
+//       <div className="btn" onClick={mint}> <img src="mint_button.svg" /> </div>:
+//       <div>
+//         {
+//           providerAvailable == null ? 
+//           ""
+//           :
+//           <div>
+//           { providerAvailable ? 
+//           <div className="btn" onClick={getAccount}> <img src="connect_wallet.svg" /> </div> : 
+//           "In order to mint, please use Chrome + Metamask"
+//         } 
+//         </div>
+//         }
+//       </div>
+//       }
+//       {
+//         transactionHash == "" ? "" :
+//         <a target="_blank" href={process.env.NEXT_PUBLIC_ETHERSCAN_URL + "/tx/" +  transactionHash}> See transaction on Etherscan</a>
 
-      }
+//       }
 
-      {
-        id == null ? 
-        <div>
-        {
-          transactionHash == "" ? "" :
-          "Waiting for transaction to land on chain..."
-        }
-        </div>
-        :
-        <div> 
-          Successfully minted #{id} - 
-          <a target="_blank" href={process.env.NEXT_PUBLIC_OPENSEA_URL + "/assets/" +  process.env.NEXT_PUBLIC_CONTRACT + "/" +  id}> View On OpenSea </a>
-        </div>
+//       {
+//         id == null ? 
+//         <div>
+//         {
+//           transactionHash == "" ? "" :
+//           "Waiting for transaction to land on chain..."
+//         }
+//         </div>
+//         :
+//         <div> 
+//           Successfully minted #{id} - 
+//           <a target="_blank" href={process.env.NEXT_PUBLIC_OPENSEA_URL + "/assets/" +  process.env.NEXT_PUBLIC_CONTRACT + "/" +  id}> View On OpenSea </a>
+//         </div>
 
-      }
-    </div>
-  )
-}
+//       }
+//     </div>
+//   )
+// }
 
